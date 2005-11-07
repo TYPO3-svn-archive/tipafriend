312a49e3429ef4ff64d18c23dd8e2bff66ec6681
kasper <kasper@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-11-07 12:27:31 +0000

    *** empty log message ***
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tipafriend/trunk@2633 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,10 +1,10 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: 'tipafriend'
-#
-# Auto generated 12-02-2003 21:18
-#
+# Extension Manager/Repository config file for ext: "tipafriend"
+# 
+# Auto generated 07-10-2005 15:45
+# 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
 ########################################################################
@@ -15,9 +15,11 @@ $EM_CONF[$_EXTKEY] = Array (
 	'category' => 'plugin',
 	'shy' => 0,
 	'dependencies' => 'cms',
-	'TYPO3_version' => '3.5.0',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '3.5.0-',
+	'PHP_version' => '-',
 	'module' => '',
 	'state' => 'stable',
 	'internal' => 0,
@@ -29,10 +31,12 @@ $EM_CONF[$_EXTKEY] = Array (
 	'author' => 'Kasper Skårhøj',
 	'author_email' => 'kasperYYYY@typo3.com',
 	'author_company' => 'Curby Soft Multimedia',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
 	'private' => 0,
 	'download_password' => '',
-	'version' => '1.0.4',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:10:{s:12:"ext_icon.gif";s:4:"3b63";s:14:"ext_tables.php";s:4:"ed39";s:28:"ext_typoscript_constants.txt";s:4:"aef9";s:24:"ext_typoscript_setup.txt";s:4:"c7a8";s:13:"locallang.php";s:4:"11b4";s:26:"pi/class.tx_tipafriend.php";s:4:"b7ca";s:23:"pi/tipafriend_help.tmpl";s:4:"6790";s:23:"pi/tipafriend_help1.gif";s:4:"3016";s:27:"pi/tipafriend_template.tmpl";s:4:"1a2a";s:30:"pi/tipafriend_template_dk.tmpl";s:4:"2d3d";}',
+	'version' => '1.2.1',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:19:{s:24:".#ext_emconf.php.1.1.1.1";s:4:"fe1a";s:9:"ChangeLog";s:4:"668e";s:12:"ext_icon.gif";s:4:"3b63";s:14:"ext_tables.php";s:4:"4637";s:28:"ext_typoscript_constants.txt";s:4:"0381";s:24:"ext_typoscript_setup.txt";s:4:"e728";s:13:"locallang.php";s:4:"11b4";s:11:"CVS/Entries";s:4:"8f45";s:14:"CVS/Repository";s:4:"9ed0";s:8:"CVS/Root";s:4:"bbeb";s:26:"pi/class.tx_tipafriend.php";s:4:"61a8";s:23:"pi/tipafriend_help.tmpl";s:4:"6790";s:23:"pi/tipafriend_help1.gif";s:4:"3016";s:27:"pi/tipafriend_template.tmpl";s:4:"dfca";s:30:"pi/tipafriend_template_dk.tmpl";s:4:"2d3d";s:14:"pi/CVS/Entries";s:4:"d063";s:17:"pi/CVS/Repository";s:4:"a8a9";s:11:"pi/CVS/Root";s:4:"bbeb";s:14:"doc/manual.sxw";s:4:"884f";}',
 );
 
 ?>
