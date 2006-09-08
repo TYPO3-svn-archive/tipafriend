76f4bf16136c2f3045ecfb0607f1b5fcb14868db
thorsten <thorsten@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-09-08 13:13:33 +0000

    revoke changes from revision 3679 until internally confirmed
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tipafriend/trunk@3683 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,14 +34,13 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.2.2',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.0.0-',
-			'php' => '4.2.0-',
+			'typo3' => '3.0.0-',
+			'php' => '3.0.0-',
 			'cms' => '',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
-			'sr_freecap' => '0.4.3-',
 		),
 	),
 	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"4124";s:12:"ext_icon.gif";s:4:"3b63";s:14:"ext_tables.php";s:4:"4637";s:28:"ext_typoscript_constants.txt";s:4:"0381";s:24:"ext_typoscript_setup.txt";s:4:"e728";s:13:"locallang.php";s:4:"11b4";s:26:"pi/class.tx_tipafriend.php";s:4:"f22a";s:22:"pi/tipafriend_help.gif";s:4:"3016";s:23:"pi/tipafriend_help.tmpl";s:4:"5a14";s:27:"pi/tipafriend_template.tmpl";s:4:"dfca";s:30:"pi/tipafriend_template_dk.tmpl";s:4:"2d3d";s:14:"doc/manual.sxw";s:4:"884f";}',
