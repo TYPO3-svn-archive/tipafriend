7c98b4473bcd4964f43add3aea6e238fc2cc14e7
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-30 12:38:07 +0000

    Fixed issue #7073: TipAFriend uses t3lib_div::fixed_lgd() which is deprecated
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tipafriend/trunk@31731 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi/class.tx_tipafriend.php
+++ pi/class.tx_tipafriend.php
@@ -154,7 +154,7 @@ class tx_tipafriend extends tslib_pibase {
 		$markerArray['###URL_ENCODED###']=rawurlencode($url);
 		$markerArray['###URL_SPECIALCHARS###']=htmlspecialchars($url);
 		if ($url)	{
-			$markerArray['###URL_DISPLAY###']=htmlspecialchars(strlen($url)>70 ? t3lib_div::fixed_lgd($url,30).t3lib_div::fixed_lgd($url,-30) : $url);
+			$markerArray['###URL_DISPLAY###'] = htmlspecialchars(strlen($url) > 70 ? t3lib_div::fixed_lgd_cs($url, 30) . t3lib_div::fixed_lgd_cs($url, -30) : $url);
 		} else {
 			// display an error if the URL was unset or if it is missing
 			$markerArray['###URL_DISPLAY###'] = '<strong style="color:red;">ERROR: malformed or missing URL detected!</strong>';
